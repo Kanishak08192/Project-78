8bce56f57a0196f1ac0f0bbd4fe3441adb35e909
Kanishak08192 <kanishak2007@gmail.com>
2021-06-09 23:37:12 +0530

    commit1

--- config.js
+++ config.js
@@ -3,14 +3,14 @@ require('@firebase/firestore');
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA-GExxb4yBuo6d7TgIViCqyCH4Zq-apb8",
-    authDomain: "book-santa-3a50a.firebaseapp.com",
-    projectId: "book-santa-3a50a",
-    storageBucket: "book-santa-3a50a.appspot.com",
-    messagingSenderId: "237111627235",
-    appId: "1:237111627235:web:bea54693c64d72984aa4d6",
-    measurementId: "G-DLRX63PD26"
-  };
+  apiKey: "AIzaSyDe3hbNlXS6FounVGQIpe0CONJQ1GACBDs",
+  authDomain: "barter-system-1dfdf.firebaseapp.com",
+  projectId: "barter-system-1dfdf",
+  storageBucket: "barter-system-1dfdf.appspot.com",
+  messagingSenderId: "569035632813",
+  appId: "1:569035632813:web:3ebc76ef51bc99a590d624",
+  measurementId: "G-RG5WB36FT4"
+};
 
 firebase.initializeApp(firebaseConfig);
 export default firebase.firestore();
